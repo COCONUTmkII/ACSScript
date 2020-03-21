63e3348967c5e0425929edbcab3f28b9ea99f838
COCONUTmkII <coconutmkii@gmail.com>
2020-03-21 14:36:57 +0300

    Added custom icon for acs file.

--- src/main/java/by/home/acs/language/ACSScriptIcon.java
+++ src/main/java/by/home/acs/language/ACSScriptIcon.java
@@ -4,5 +4,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class ACSScriptIcon {
-    public static final Icon FILE = IconLoader.getIcon("/icons/acs.png");
+    public static final Icon FILE = IconLoader.getIcon("/icons/acs_icon.png");
 }
