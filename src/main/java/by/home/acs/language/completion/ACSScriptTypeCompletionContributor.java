f3b6ae71585c40e7f4f69389f83413c67df0bd76
COCONUTmkII <coconutmkii@gmail.com>
2020-07-04 18:33:34 +0300

    Added script type basic completion contributor and basic find usages contributor.

---
+++ src/main/java/by/home/acs/language/completion/ACSScriptTypeCompletionContributor.java
@@ -0,0 +1,33 @@
+package by.home.acs.language.completion;
+
+import by.home.acs.language.ACSScriptLanguage;
+import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.patterns.PlatformPatterns;
+import com.intellij.util.ProcessingContext;
+import org.jetbrains.annotations.NotNull;
+
+public class ACSScriptTypeCompletionContributor extends CompletionContributor {
+    public ACSScriptTypeCompletionContributor() {
+        extend(CompletionType.BASIC, PlatformPatterns.psiElement().withLanguage(ACSScriptLanguage.INSTANCE), new CompletionProvider<CompletionParameters>() {
+            @Override
+            protected void addCompletions(@NotNull CompletionParameters parameters, @NotNull ProcessingContext context, @NotNull CompletionResultSet result) {
+                result.addElement(LookupElementBuilder.create("OPEN"));
+                result.addElement(LookupElementBuilder.create("ENTER"));
+                result.addElement(LookupElementBuilder.create("RETURN"));
+                result.addElement(LookupElementBuilder.create("DEATH"));
+                result.addElement(LookupElementBuilder.create("RESPAWN"));
+                result.addElement(LookupElementBuilder.create("LIGHTNING"));
+                result.addElement(LookupElementBuilder.create("UNLOADING"));
+                result.addElement(LookupElementBuilder.create("KILL"));
+                result.addElement(LookupElementBuilder.create("REOPEN"));
+                result.addElement(LookupElementBuilder.create("DISCONNECT"));
+            }
+        });
+    }
+
+    @Override
+    public void fillCompletionVariants(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
+        super.fillCompletionVariants(parameters, result);
+    }
+}
---
+++ src/main/java/by/home/acs/language/usage/ACSScriptUsageProvider.java
@@ -0,0 +1,65 @@
+package by.home.acs.language.usage;
+
+import by.home.acs.language.ACSScriptLexerAdapter;
+import by.home.acs.language.ACSScriptTypes;
+import by.home.acs.language.psi.ACSScriptDefinition;
+import com.intellij.lang.cacheBuilder.DefaultWordsScanner;
+import com.intellij.lang.cacheBuilder.WordsScanner;
+import com.intellij.lang.findUsages.FindUsagesProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiNamedElement;
+import com.intellij.psi.tree.TokenSet;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class ACSScriptUsageProvider implements FindUsagesProvider {
+    @Nullable
+    @Override
+    public WordsScanner getWordsScanner() {
+        return new DefaultWordsScanner(new ACSScriptLexerAdapter(),
+                TokenSet.create(ACSScriptTypes.DEFINITION),
+                TokenSet.create(ACSScriptTypes.COMMENT),
+                TokenSet.EMPTY);
+    }
+
+    @Override
+    public boolean canFindUsagesFor(@NotNull PsiElement psiElement) {
+        return psiElement instanceof PsiNamedElement;
+    }
+
+    @Nullable
+    @Override
+    public String getHelpId(@NotNull PsiElement psiElement) {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public String getType(@NotNull PsiElement element) {
+        if (element instanceof ACSScriptDefinition) {
+            return "ACS Script File";
+        } else {
+            return "";
+        }
+    }
+
+    @NotNull
+    @Override
+    public String getDescriptiveName(@NotNull PsiElement element) {
+        if (element instanceof ACSScriptDefinition) {
+            return element.getText();
+        } else {
+            return "";
+        }
+    }
+
+    @NotNull
+    @Override
+    public String getNodeText(@NotNull PsiElement element, boolean useFullName) {
+        if (element instanceof ACSScriptDefinition) {
+            return element.getText(); //TODO Must Create Good Annotator here.
+        } else {
+            return "";
+        }
+    }
+}
